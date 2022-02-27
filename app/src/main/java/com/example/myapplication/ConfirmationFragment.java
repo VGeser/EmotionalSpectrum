0992be5e02b44f6206a71e0c039d86fe04888638
VGeser <50826449+VGeser@users.noreply.github.com>
2022-02-27 21:22:20 +0700

    Working fragment + visible coordinates

--- app/src/main/java/com/example/myapplication/ConfirmationFragment.java
+++ app/src/main/java/com/example/myapplication/ConfirmationFragment.java
@@ -7,38 +7,28 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
-
-public class ConfirmationFragment extends DialogFragment implements CurrentValue{
+//implements CurrentValue
+public class ConfirmationFragment extends DialogFragment {
     private String out_text;
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        //((MainActivity) getActivity()).setCurrentValue(ConfirmationFragment.this);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage(R.string.dialog_message)
-                .setPositiveButton(R.string.agree, new DialogInterface.OnClickListener() {
+        builder.setMessage(R.string.dialog_message);
+        builder.setPositiveButton(R.string.agree, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                    }
-                })
-                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // User cancelled the dialog
+                        ((MainActivity) getActivity()).doPositiveClick();
                     }
                 });
-        // Create the AlertDialog object and return it
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                ((MainActivity) getActivity()).doNegativeClick();
+            }
+        });
+
+        builder.create();
         return builder.create();
     }
 
-    @Override
-    public void setCurrentMood(byte id) {
-        switch (id){
-            case 0:
-                out_text = "For case 0";
-            case 1:
-                out_text = "For case 1";
-            default:
-                out_text = "For default";
-        }
-    }
 }
