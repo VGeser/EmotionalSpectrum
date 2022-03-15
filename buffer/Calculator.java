40e454615e5a365cf1a00b48f0bc6c7baad5efd2
vlada967 <70951081+vlada967@users.noreply.github.com>
2022-03-15 13:10:56 +0700

    Update and rename Function.java to Calculator.java

--- buffer/Calculator.java
+++ buffer/Calculator.java
@@ -9,11 +9,12 @@ public static int[] getScreenSize(Context context) {
     }
 
 
-public class Function
+public class Calculator
 {	
-	public static float func (float x, float y) {
-	    int S;
+	public static double calculator (float x, float y) {
+	    int S = 0, Int = 0;
 	    int R = getScreenSize() / 2;
+	    double ID = 0;
 
 	    if (Math.pow((x - R), 2) + Math.pow((y + R), 2) < Math.pow((R), 2)) {
 
@@ -53,8 +54,39 @@ public class Function
 	                S = 17;
 	            }
 	        }
+
+	        if (Math.pow((x - R), 2) + Math.pow((y + R), 2) < (Math.pow((R), 2)) / 10) {
+	        	Int = 10;
+	        }
+	        else if (Math.pow((x - R), 2) + Math.pow((y + R), 2) < (2 * Math.pow((R), 2)) / 10) {
+	        	Int = 9;
+	        }
+	        else if (Math.pow((x - R), 2) + Math.pow((y + R), 2) < (3 * Math.pow((R), 2)) / 10) {
+	        	Int = 8;
+	        }
+	        else if (Math.pow((x - R), 2) + Math.pow((y + R), 2) < (4 * Math.pow((R), 2)) / 10) {
+	        	Int = 7;
+	        }
+	        else if (Math.pow((x - R), 2) + Math.pow((y + R), 2) < (5 * Math.pow((R), 2)) / 10) {
+	        	Int = 6;
+	        }
+	        else if (Math.pow((x - R), 2) + Math.pow((y + R), 2) < (6 * Math.pow((R), 2)) / 10) {
+	        	Int = 5;
+	        }
+	        else if (Math.pow((x - R), 2) + Math.pow((y + R), 2) < (7 * Math.pow((R), 2)) / 10) {
+	        	Int = 4;
+	        }
+	        else if (Math.pow((x - R), 2) + Math.pow((y + R), 2) < (8 * Math.pow((R), 2)) / 10) {
+	        	Int = 3;
+	        }
+	        else if (Math.pow((x - R), 2) + Math.pow((y + R), 2) < (9 * Math.pow((R), 2)) / 10) {
+	        	Int = 2;
+	        }
+	        else {
+	        	Int = 1;
+	        }
 	    }
-	    //res = (x + y) * 0.36 * S * Int;
-	    return 0;
+	    ID = (x + y) * 0.36 * S * Int;
+	    return ID;
 	}
 }
